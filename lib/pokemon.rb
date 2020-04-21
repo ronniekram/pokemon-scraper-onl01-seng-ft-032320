5887e4dfd521ea14a5204b3efaaecebc21a70649
Arynn Boniface <arynn.boniface@gmail.com>
2020-04-21 18:51:39 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -11,7 +11,7 @@ class Pokemon
     @@all << self
   end
   
-  def self.save(name, type,db)
+  def self.save(name, type, db)
     db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
     
     @id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
