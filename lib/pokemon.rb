0e429071fd7401226064a015082554ebee3b0c52
Arynn Boniface <arynn.boniface@gmail.com>
2020-04-21 18:48:08 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -17,7 +17,7 @@ class Pokemon
     @id = @db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
   end
   
-  def self.find(id)
+  def self.find(id, db)
     sql = <<-SQL
       SELECT * 
       FROM pokemon 
